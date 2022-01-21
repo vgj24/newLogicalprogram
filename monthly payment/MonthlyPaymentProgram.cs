dc564baf7a399c9b87da32eeda79f3176ee87ad9
vgj24 <vrushlijoshi24@gmail.com>
2022-01-21 16:55:55 +0530

    monthly payment

---
+++ monthly payment/MonthlyPaymentProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthlyPayment
+{
+    internal class MonthlyPaymentProgram
+    {
+            public static void CalculateMonthlyPayment()
+            {
+            Console.WriteLine("Enter principal loan ::");
+            int Payment = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of years ::");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter rate of interest ::");
+            int Rate = Convert.ToInt32(Console.ReadLine());
+
+
+            double principal = Payment, numberOfYears = Year, rateOfInterest = Rate;
+                double payment;
+                double rate = rateOfInterest / 1200;
+                double n = 12 * Year;
+                payment = (principal * rate) / (1 - Math.Pow((1 + rate), (-n)));
+                Console.WriteLine("Monthly payment amount is :: INR {0}", payment);
+            }
+    }
+}
---
+++ monthly payment/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MonthlyPayment;
+Console.WriteLine("MONTHLY PAYMENT PROGRM");
+
+MonthlyPaymentProgram.CalculateMonthlyPayment();
